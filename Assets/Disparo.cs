db16f312876845108eba690ea0392a558fe5a82e
Rtorrerso <79935998+Rtorrerso@users.noreply.github.com>
2021-05-02 20:46:18 -0500

    version 1.0
    Arquero importado de Mixamo parte inicial tutoriales en : https://drive.google.com/file/d/1RF_IIWZiXRXisS2gDoNoFX3HuRLnRNNO/view?usp=sharing y https://drive.google.com/file/d/1RG4RdO52LVX0yJhRDewasIDKdAXudZe6/view?usp=sharing

---
+++ Assets/Disparo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public GameObject flecha;
+    public GameObject flecha2;
+    public Transform  NDisparo;
+    public float vel;
+    public Animator FAnimator;
+    
+    
+        
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        vel = 1000f;
+        FAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1"))
+        {
+            FAnimator.CrossFadeInFixedTime("Shoot",0.1f);
+            //FDisparo();
+           
+
+
+            
+        }
+    }
+
+    public void FDisparo()
+    {
+            GameObject Nflecha =  Instantiate(flecha,NDisparo.position,NDisparo.rotation);
+            Nflecha.transform.Rotate(-90,180,0);
+            
+            //var RBFlecha = Nflecha.GetComponent<Rigidbody>();
+            var RBFlecha = Nflecha.GetComponent<Rigidbody>();
+            RBFlecha.AddForce(0,0,vel*Time.deltaTime,ForceMode.Impulse);
+
+
+
+        
+    }
+
+}
---
+++ Assets/logicaFlecha.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logicaFlecha : MonoBehaviour
+{
+
+    public GameObject flecha2;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag=="diana")
+        {
+            Debug.Log("choque con: " +other.gameObject.name);
+            GameObject NewFlecha = Instantiate(flecha2,this.transform.position,this.transform.rotation) as GameObject;
+            Destroy(this.gameObject,0.0f);
+        }    
+    }
+                    
+}
